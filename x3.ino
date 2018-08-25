7d3344768d63c97f7f0468097aa861ed60b0a8d1
Peerarust Siriamphan <peerarust.si@tnis.com>
2018-08-25 18:36:50 +0700

    Control Voltage by Serial

---
+++ x3.ino
@@ -0,0 +1,29 @@
+String incomingByte;
+int motor = 7;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+  Serial.println("Please Input 0 - 255");
+  analogWrite(7, 0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.readString();
+    int power = incomingByte.toInt();
+    Serial.println(incomingByte);
+    analogWrite(7, power);
+    Serial.println("DONE!!!");
+  }
+
+}
